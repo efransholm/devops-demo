51edb39214a982951e466217ff60840ccc7aae4d
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-08-29 15:54:32 +0200

    Add buttons for increase/decrease date

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
   });
 
   const currentDate = dayjs();
-  const [chosenDate, setChosenDate] = useState<dayjs.Dayjs | null>(currentDate);
+  const [chosenDate, setChosenDate] = useState<dayjs.Dayjs>(currentDate);
   const [easterEggMessage, setEasterEggMessage] = useState<string | null>(null);
 
   useEffect(() => {
@@ -29,7 +29,29 @@ function App() {
     <Container>
       <Title>Find out which week your date belongs to</Title>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker label='Basic date picker' onChange={(newDate) => setChosenDate(newDate)} value={chosenDate} />
+        <DateContainer>
+          <StyledButton
+            onClick={() => {
+              const newDate = decreaseDate(chosenDate);
+              newDate && setChosenDate(newDate);
+            }}
+          >
+            -
+          </StyledButton>
+          <DatePicker
+            label='Basic date picker'
+            onChange={(newDate) => newDate && setChosenDate(newDate)}
+            value={chosenDate}
+          />
+          <StyledButton
+            onClick={() => {
+              const newDate = increaseDate(chosenDate);
+              newDate && setChosenDate(newDate);
+            }}
+          >
+            +
+          </StyledButton>
+        </DateContainer>
         <div>
           {chosenDate ? `The date ${chosenDate.format('YYYY-MM-DD')} belongs to week:` : 'Please select a date.'}
           {chosenDate && <Week>{chosenDate.isoWeek()}</Week>}
@@ -38,6 +60,14 @@ function App() {
       </LocalizationProvider>
     </Container>
   );
+}
+
+export function decreaseDate(newDate: dayjs.Dayjs) {
+  if (newDate) return newDate.subtract(1, 'day');
+}
+
+export function increaseDate(newDate: dayjs.Dayjs) {
+  if (newDate) return newDate.add(1, 'day');
 }
 
 const Container = styled.div`
@@ -53,6 +83,31 @@ const Container = styled.div`
 const Title = styled.h1`
   font-size: 50px;
   max-width: 600px;
+`;
+
+const DateContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+  align-items: center;
+`;
+
+const StyledButton = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #42bbf8ff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 20px;
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #1e90ffff;
+  }
 `;
 
 const Week = styled.h2`
