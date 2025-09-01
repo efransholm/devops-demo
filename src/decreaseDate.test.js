5d788d67bde3a950d487259edc19ab41201f7f3f
Elin Fransholm <fransholm@n134-p164.eduroam.kth.se>
2025-09-01 10:23:30 +0200

    feat: Add 2 more tests

--- src/decreaseDate.test.js
+++ src/decreaseDate.test.js
@@ -1,9 +1,20 @@
-import dayjs from 'dayjs';
-import { test, expect } from 'vitest';
-import { decreaseDate } from './App';
+import dayjs from "dayjs";
+import { test, expect, beforeAll } from "vitest";
+import { decreaseDate } from "./App";
+import isoWeek from "dayjs/plugin/isoWeek";
 
-test('decreaseDate', () => {
-  const testDate = dayjs('2025-08-29');
+beforeAll(() => {
+  dayjs.extend(isoWeek);
+});
+
+test("test decreasing date", () => {
+  const testDate = dayjs("2025-08-29");
   const decreasedDate = decreaseDate(testDate);
-  expect(decreasedDate.isSame(dayjs('2025-08-28'))).toBe(true);
+  expect(decreasedDate.isSame(dayjs("2025-08-28"))).toBe(true);
+});
+
+test("test week changes when decreasing date on a monday", () => {
+  const testDate = dayjs("2018-01-01");
+  const decreasedDate = decreaseDate(testDate);
+  expect(testDate.isoWeek()).toBeLessThan(decreasedDate.isoWeek());
 });
--- src/increaseDate.test.js
+++ src/increaseDate.test.js
@@ -1,9 +1,20 @@
-import dayjs from 'dayjs';
-import { test, expect } from 'vitest';
-import { increaseDate } from './App';
+import dayjs from "dayjs";
+import { test, expect, beforeAll } from "vitest";
+import { increaseDate } from "./App";
+import isoWeek from "dayjs/plugin/isoWeek";
 
-test('decreaseDate', () => {
-  const testDate = dayjs('2025-08-29');
+beforeAll(() => {
+  dayjs.extend(isoWeek);
+});
+
+test("decreaseDate", () => {
+  const testDate = dayjs("2025-08-29");
   const increasedDate = increaseDate(testDate);
-  expect(increasedDate.isSame(dayjs('2025-08-30'))).toBe(true);
+  expect(increasedDate.isSame(dayjs("2025-08-30"))).toBe(true);
+});
+
+test("test week changes when increasing date on a sunday", () => {
+  const testDate = dayjs("2018-01-21");
+  const decreasedDate = increaseDate(testDate);
+  expect(testDate.isoWeek()).toBeLessThan(decreasedDate.isoWeek());
 });
